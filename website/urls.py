42415352d7604f6550c43dbde622ee0aa7783a9b
Dan <yo@gmail.om>
2025-02-18 16:28:58 +0100

    add landing page and base template for Ereader

--- website/urls.py
+++ website/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
+    path("", views.landing_page, name="landing_page"),
     path("ereader/", include("ereader.urls")),
     path("admin/", admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path("accounts/", include("allauth.urls")),
 ]
